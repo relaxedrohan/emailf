f62fbc4c390477aa6e570abcf8fb5f368733e793
rohanyadav420 <yaduvanshirohan3@gmail.com>
2020-11-22 13:33:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport'); 
+
+
+require('./models/users');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+
+app.use(
+	cookieSession({
+		maxAge : 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+  
+
+
