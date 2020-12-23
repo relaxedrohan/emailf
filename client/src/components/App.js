4d19626a1253a7c3cbca315cfb5ded7dfd90dab5
rohanyadav420 <yaduvanshirohan4@gmail.com>
2020-12-23 23:09:34 +0530

    modified changes

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const App = () => {
+
+	return (
+		<div>
+			Hi there 
+		</div>
+		);
+	};
+
+export default App; 
--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,16 @@ passport.use(
 			callbackURL : '/auth/google/callback',
 			proxy : true
 		}, 
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleID: profile.id})
-			.then((existingUser)=> {
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleID: profile.id})
 				if (existingUser){
-					done(null, existingUser);
+					return done(null, existingUser);
 
-				} else {
-					new User({ googleID : profile.id })
-					.save()
-					.then( user => done(null, user));
 				}
-				});
+				const user = await new User({ googleID : profile.id }).save();
+				done(null, user);
+				
+				
 			}
 		
 	 ));
