54d018779fbd497c8b80f4e27f5de4d0eebbd50d
rohanyadav420 <yaduvanshirohan4@gmail.com>
2021-01-27 16:56:20 +0530

    Pre Payment

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = ()=>{
+    return (
+        <div style = {{ textAlign : 'center'}}>
+            <h1>
+                Emailf!
+            </h1>
+            Collect valuable feedback from your customers.
+        </div>
+    );
+};
+
+export default Landing;
+
