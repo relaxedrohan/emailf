1a3845cf27a195f18b67345df14f6ef35b9b44e5
rohanyadav420 <yaduvanshirohan4@gmail.com>
2021-02-03 00:34:52 +0530

    deployment issues

--- index.js
+++ index.js
@@ -30,11 +30,12 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if(process.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV ===  'production'){
+
 	app.get(express.static('client/build'));
 
 	const path = require('path');
-	app.get('/*', (req, res)=>{
+	app.get('*', (req, res)=>{
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
